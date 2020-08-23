7b8471158672a9becd24d2dda36c35a0252cebf4
1-karthigeyan-1 <karthigeyan98@yahoo.com.sg>
2020-08-23 22:01:11 +0800

    Add List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
 
@@ -9,22 +10,35 @@ public class Duke {
         System.out.println(line + greet + line);
     }
 
-    public static void echoCommand(String command) {
-        System.out.println(line + command + "\n" + line);
-    }
-
     public static void exitCommand(String command) {
         String exit = "Bye. Hope to see you again soon!\n";
         System.out.println(line + exit + line);
+    }
+    public static void showList(String[] storeText) {
+        System.out.println(line);
+        int itemCount = 1;
+        for (String item : storeText) {
+            System.out.println(itemCount + ". " + item );
+            itemCount++;
+        }
+        System.out.println(line);
     }
 
     public static void main(String[] args) {
         startGreeting();
         String command;
+        String[] storeText = new String[100];
+        int textCount = 0;
         Scanner in = new Scanner(System.in);
-
+        //Check for exit command
         while (!((command = in.nextLine()).equals("bye"))) {
-            echoCommand(command);
+            if (command.equals("list")) {
+                showList(Arrays.copyOf(storeText, textCount));
+            } else {
+                storeText[textCount] = command;
+                textCount++;
+                System.out.println(line + "added: " + command + "\n" + line);
+            }
         }
 
         exitCommand(command);
