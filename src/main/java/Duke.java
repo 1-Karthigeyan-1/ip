75cb7197dd643185adb215f19681bc4b24cf712d
1-karthigeyan-1 <karthigeyan98@yahoo.com.sg>
2020-08-23 18:25:14 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String line = "____________________________________________________________\n";
+
+    private static String line = "____________________________________________________________\n";
+
+    public static void startGreeting() {
         String greet = "Hello! I'm Duke\nWhat can I do for you?\n";
+        System.out.println(line + greet + line);
+    }
+
+    public static void echoCommand(String command) {
+        System.out.println(line + command + "\n" + line);
+    }
+
+    public static void exitCommand(String command) {
         String exit = "Bye. Hope to see you again soon!\n";
-        System.out.println(line + greet + line + exit + line);
+        System.out.println(line + exit + line);
+    }
+
+    public static void main(String[] args) {
+        startGreeting();
+        String command;
+        Scanner in = new Scanner(System.in);
+
+        while (!((command = in.nextLine()).equals("bye"))) {
+            echoCommand(command);
+        }
+
+        exitCommand(command);
     }
 }
