f8aa799e1df162227417e89442b7c1c3dc0f75dc
1-karthigeyan-1 <karthigeyan98@yahoo.com.sg>
2020-08-26 23:21:30 +0800

    modify to coding standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,62 +3,62 @@ import java.util.Arrays;
 
 public class Duke {
 
-    private static String line = "____________________________________________________________";
-
     public static void main(String[] args) {
-        startGreeting();
         String command;
         Task[] storeTask = new Task[100];
         int textCount = 0;
         Scanner in = new Scanner(System.in);
+
+        startGreeting();
+
         //Check for exit command
         while (!((command = in.nextLine()).equals("bye"))) {
             if (command.equals("list")) {
                 showList(Arrays.copyOf(storeTask, textCount));
-            }
-            else if (command.contains("done")) {
-                String[] splitString = command.split(" ");
-                int taskNumber = Integer.parseInt(splitString[1]);
-                storeTask[taskNumber - 1].isCompleted();
-                System.out.println(line);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println("[" + storeTask[taskNumber - 1].getStatusIcon() + "] " + storeTask[taskNumber - 1].getDescription() );
-                System.out.println(line);
-
+            } else if (command.contains("done")) {
+                completeTask(storeTask, command);
             } else {
                 storeTask[textCount] = new Task(command);
-                System.out.println(line);
-                System.out.println("added: " + storeTask[textCount].getDescription());
-                System.out.println(line);
+                textSeparator( "added: " + storeTask[textCount].getDescription() + "\n");
                 textCount++;
             }
         }
-
         exitCommand(command);
     }
 
+    public static void textSeparator(String displayText) {
+        String line = "____________________________________________________________\n";
+        System.out.print(line + displayText + line);
+    }
+
     public static void startGreeting() {
-        String greet = "Hello! I'm Duke\nWhat can I do for you?";
-        System.out.println(line);
-        System.out.println(greet);
-        System.out.println(line);
+        String greet = "Hello! I'm Duke\nWhat can I do for you?\n";
+        textSeparator(greet);
     }
 
     public static void exitCommand(String command) {
         String exit = "Bye. Hope to see you again soon!\n";
-        System.out.println(line);
-        System.out.println(exit);
-        System.out.println(line);
+        textSeparator(exit);
     }
 
     public static void showList(Task[] storeText) {
         int itemCount = 1;
-        System.out.println(line);
-        System.out.println("Here are the tasks in your list:");
+        String itemList = "";
         for (Task item : storeText) {
-            System.out.println(itemCount + ".[" + item.getStatusIcon() + "]" + item.getDescription() );
+            itemList += Integer.toString(itemCount) + ".[" + item.getStatusIcon()
+                    + "]" + item.getDescription() + "\n";
             itemCount++;
         }
-        System.out.println(line);
+        textSeparator("Here are the tasks in your list:\n" + itemList);
     }
+
+    public static void completeTask(Task[] storeTask,String command) {
+        String[] splitString = command.split(" ");
+        int taskNumber = Integer.parseInt(splitString[1]) - 1;
+        Task taskItem = storeTask[taskNumber];
+        taskItem.isCompleted();
+        textSeparator("Nice! I've marked this task as done:\n" + "["
+                + taskItem.getStatusIcon() + "] " + taskItem.getDescription() + "\n");
+    }
+
 }
