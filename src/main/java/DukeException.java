9257922c2723825f51203aea7bb1f028bcda48be
1-karthigeyan-1 <karthigeyan98@yahoo.com.sg>
2020-09-09 09:40:46 +0800

    Add DukeException class

---
+++ src/main/java/DukeException.java
@@ -0,0 +1,15 @@
+public class DukeException extends Exception {
+    protected static final String EMPTY_LIST = "empty list";
+    protected static final String ILLEGAL_NUMBER = "illegal number";
+
+    public DukeException(String error) {
+        switch(error) {
+        case EMPTY_LIST:
+            Duke.printBorder("List is empty!!\n");
+            break;
+        case ILLEGAL_NUMBER:
+            Duke.printBorder("Please input a valid number\n");
+            break;
+        }
+    }
+}
