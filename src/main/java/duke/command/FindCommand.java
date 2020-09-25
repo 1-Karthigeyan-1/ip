b4163ad0ca421aded27ee93f1a30c8e2a7b34bf5
1-karthigeyan-1 <karthigeyan98@yahoo.com.sg>
2020-09-25 21:30:12 +0800

    Update Javadoc in find class

--- src/main/java/duke/command/FindCommand.java
+++ src/main/java/duke/command/FindCommand.java
@@ -2,12 +2,21 @@ package duke.command;
 
 import duke.Duke;
 
+/**
+ * Represents the find command used in the user input
+ */
 public class FindCommand extends CommandArgument{
     public static final String COMMAND_WORD = "find";
+
     public FindCommand(String description) {
         super(description);
     }
 
+    /**
+     * Executes the find command by showing the list of matching tasks in program.
+     *
+     * @param description keyword used to search for task
+     */
     @Override
     public void execute(String description) {
         Duke.getTaskList().lookupTask(description);
